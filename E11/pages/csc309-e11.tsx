59e718ce351009be034130bd69ae771286a936f2
Lily Xiaomei Zhou <lilyxm.zhou@mail.utoronto.ca>
2024-11-28 10:45:05 -0500

    created app

---
+++ E11/pages/csc309-e11.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Csc309E11: React.FC = () => {
+  return (
+    <div id="auto-tester">
+      Hi. I am zhoulil5, and I have learned how to develop a full-stack web app
+      and deploy it!
+    </div>
+  );
+};
+
+export default Csc309E11;
